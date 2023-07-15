7dcb1ad008bab40fd2e23948b32df53972ef5bcf
AJ Horst <AJ_Horst@intuit.com>
2023-07-15 18:32:52 -0400

    wip;

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">Wedding!</header>
-            <h3>Coming August 24, 2024</h3>
+            <div className="App-header">
+                <h1>Wedding!</h1>
+                <h3>Coming August 24, 2024</h3>
+                <span className="secret">sluts</span>
+            </div>
         </div>
     );
 }
